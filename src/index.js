db6aefe1469a0a43a63d10514e74fb3452e3709f
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-05 10:38:16 +1100

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Yingzheng";
+const lName = "Wang";
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fName} ${lName}`}!</h1>
+    <p>Today is {Math.floor(Math.random()*10)}</p>
+  </div>, 
+  document.getElementById("root"));
